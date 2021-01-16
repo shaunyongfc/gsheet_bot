97580931117ff3814e0849a90c51b478f0c79db2
Shaun <yongfongchuan@gmail.com>
2021-01-16 11:55:46 +1100

    move spreadsheet

--- gsheet_handler.py
+++ gsheet_handler.py
@@ -6,33 +6,33 @@ scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
 client = gspread.authorize(creds)
-testspreadsheet = client.open("Octopath WOTV")
-sharedspreadsheet = client.open("Ramada Bot")
+myspreadsheet = client.open("Octopath WOTV")
+ramadaspreadsheet = client.open("Ramada Bot")
 
 class DfHandlerWotv():
     # Object handling WOTV sheets related operations
     def __init__(self):
         self.sync()
     def sync(self):
-        df = pd.DataFrame(testspreadsheet.worksheet('WOTV_eq').get_all_records())
+        df = pd.DataFrame(myspreadsheet.worksheet('WOTV_eq').get_all_records())
         self.eq = df.set_index('EQ Name')
 
-        df = pd.DataFrame(testspreadsheet.worksheet('WOTV_matname').get_all_records())
-        self.mat = df.set_index('Material')
-
-        df = pd.DataFrame(testspreadsheet.worksheet('WOTV_vc').get_all_records())
+        df = pd.DataFrame(myspreadsheet.worksheet('WOTV_vc').get_all_records())
         self.vc = df.set_index('VC Name')
 
-        df = pd.DataFrame(testspreadsheet.worksheet('WOTV_esper').get_all_records())
+        df = pd.DataFrame(myspreadsheet.worksheet('WOTV_esper').get_all_records())
         self.esper = df.set_index('Esper')
 
-        df = pd.DataFrame(testspreadsheet.worksheet('WOTVGL_esper').get_all_records())
+        df = pd.DataFrame(myspreadsheet.worksheet('WOTVGL_esper').get_all_records())
         self.glesper = df.set_index('Esper')
 
-        df = pd.DataFrame(sharedspreadsheet.worksheet('WOTV_shortcut').get_all_records())
+        df = pd.DataFrame(ramadaspreadsheet.worksheet('WOTV_matname').get_all_records())
+        self.mat = df.set_index('Material')
+
+        df = pd.DataFrame(ramadaspreadsheet.worksheet('WOTV_shortcut').get_all_records())
         self.shortcut = df.set_index('Shortcut')
 
-        self.stars = pd.DataFrame(sharedspreadsheet.worksheet('WOTV_stars').get_all_records())
+        self.stars = pd.DataFrame(ramadaspreadsheet.worksheet('WOTV_stars').get_all_records())
 
 dfwotv = DfHandlerWotv()
 
@@ -42,12 +42,12 @@ class DfHandlerGen():
         self.res = re.compile(r'&\w+') # regex for shortcuts
         self.sync()
     def sync(self):
-        self.shortcuts = pd.DataFrame(sharedspreadsheet.worksheet('my_shortcuts').get_all_records())
+        self.shortcuts = pd.DataFrame(ramadaspreadsheet.worksheet('my_shortcuts').get_all_records())
     def add_shortcut(self, *arg):
         if len(arg) == 3:
             try:
                 int(arg[2])
-                sharedspreadsheet.worksheet('my_shortcuts').append_row(list(arg))
+                ramadaspreadsheet.worksheet('my_shortcuts').append_row(list(arg))
                 self.sync()
                 return 'Added.'
             except:
@@ -83,7 +83,7 @@ class DfHandlerCotc():
     def __init__(self):
         self.sync()
     def sync(self):
-        df_cotc = pd.DataFrame(testspreadsheet.worksheet('COTC_owned').get_all_records())
+        df_cotc = pd.DataFrame(myspreadsheet.worksheet('COTC_owned').get_all_records())
         self.cotc = df_cotc.set_index('トラベラー')
 
 dfcotc = DfHandlerCotc()
