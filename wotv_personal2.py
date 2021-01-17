1101e2dbe43a8c8811a3a502cae18c3cd6f40447
Shaun <yongfongchuan@gmail.com>
2021-01-17 21:15:08 +1100

    personal 2

--- wotv_personal2.py
+++ wotv_personal2.py
@@ -1,76 +1,65 @@
 import pandas as pd
 import gspread
-from gsheet_handler import  myspreadsheet
-
+from gsheet_handler import myspreadsheet
 SHARDS_REQ = {
-    'UR+': [40, 80, 120, 160, 200],
-    'UR': [40, 80, 120, 160, 200],
-    'SSR': [20, 40, 80, 160, 200],
-    'EX': [80, 120, 200]
+    'UR+': [40, 80, 120, 160, 200, 80, 120, 200],
+    'UR': [40, 80, 120, 160, 200, 80, 120, 200],
+    'SSR': [20, 40, 80, 160, 200, 80, 120, 200]
     }
-STAGE_NAME = ['LB +0', 'LB +1', 'LB +2', 'LB +3', 'LB +4', 'LB +5', 'EX 19', 'EX 22', 'EX 25']
+SHARDS_DAILY = {'UR+': 1, 'UR': 2, 'SSR': 3}
+STAGE_NAME = ['LB +0', 'LB +1', 'LB +2', 'LB +3', 'LB +4', 'LB +5', 'J 19', 'J 22', 'J 25']
 LB_SUPPRESS = 3
 
-# WIP to process a personal sheet of unit data into progress prediction
-
-class Unit:
-    def __init__(self, rarity='UR', plus=0, shards=0):
-        self.rarity = rarity
-        self.plus = plus
-        self.shards = shards
-        self.barracks = 0
-        self.hq = 0
-        self.advance()
-    def advance(self):
-        if self.plus < 5:
-            self.checkpoint = SHARDS_REQ[self.rarity][self.plus]
-        elif self.plus == 8:
-            self.checkpoint = 0
-        else:
-            self.checkpoint = SHARDS_REQ['EX'][self.plus - 5]
-    def gain_shard(self, number):
-        if self.plus < 8:
-            self.shards += number
-            if self.shards >= self.checkpoint:
-                self.shards -= self.checkpoint
-                self.plus += 1
-                self.advance()
-                if self.plus >= LB_SUPPRESS:
-                    return 1
-        return 0
+# process personal sheets of unit data into progress prediction
 
 class Roster:
-    def __init__(self, unit_list, freq=1):
-        self.bcount = 0
-        self.hqcount = 0
+    def __init__(self, spreadsheet):
+        self.unitlb = pd.DataFrame(spreadsheet.worksheet('WOTV_unitlb').get_all_records()).set_index('Unit')
+        unithq = pd.DataFrame(spreadsheet.worksheet('WOTV_unithq').get_all_records())
+        self.unitbr = unithq[unithq['Type'] == 0]
+        self.unithq = unithq[unithq['Type'] == 1]
+        self.brqueue = -1
+        self.hqqueue = -1
         self.day_count = 0
-        self.freq = freq
-        self.units = unit_list
-        self.gains = dict()
-        for k in unit_list:
-            self.gains[k] = [0, 0]
-    def hq_print(self):
-        print(f'HQ ({self.hqcount}/10): {self.hqlist}')
-    def b_print(self):
-        print(f'Barracks ({self.bcount}/5): {self.blist}')
+    def unit_gain(self, name, gain):
+        self.unitlb.loc[name, 'Shards'] += gain
+        if self.unitlb.loc[name, 'Shards'] >= SHARDS_REQ[self.unitlb.loc[name, 'Rarity']][self.unitlb.loc[name, 'Stage']]:
+            self.unitlb.loc[name, 'Shards'] -= SHARDS_REQ[self.unitlb.loc[name, 'Rarity']][self.unitlb.loc[name, 'Stage']]
+            self.unitlb.loc[name, 'Stage'] += 1
+            return 1
+        else:
+            return 0
     def daily(self):
-        change_list = []
-        for k, v in self.units.items():
-            if v.gain_shard(sum(self.gains[k])):
-                change_list.append(k)
-        return change_list
-    def progress(self):
-        change_list = []
-        while True:
-            for _ in range(self.freq):
-                self.day_count += 1
-                change_list += self.daily()
-            if len(change_list) > 0:
-                break
-        print(f'Day {self.day_count}:')
-        for name in change_list:
-            print(f'   {name} -> {STAGE_NAME[self.units[name].plus]}!')
-        return self.day_count
-    def progress_target(self, name, target):
-        while self.units[name].plus < target:
-            self.progress()
+        self.day_count += 1
+        brcount = 0
+        for index, row in self.unitbr.iterrows():
+            if self.unitlb.loc[row['Unit'], 'Stage'] < row['Target']:
+                brcount += 1
+                if index > self.brqueue:
+                    self.brqueue = index
+                    print(f" - Barracks Update: {row['Unit']}")
+                if self.unit_gain(row['Unit'], SHARDS_DAILY[self.unitlb.loc[row['Unit'], 'Rarity']]):
+                    if self.unitlb.loc[row['Unit'], 'Stage'] >= LB_SUPPRESS:
+                        print(f"Day {self.day_count}: {row['Unit']} progressed into {STAGE_NAME[self.unitlb.loc[row['Unit'], 'Stage']]}")
+                if brcount == 5:
+                    break
+        hqcount = 0
+        for index, row in self.unithq.iterrows():
+            if self.unitlb.loc[row['Unit'], 'Stage'] < row['Target']:
+                hq_gain = min(SHARDS_DAILY[self.unitlb.loc[row['Unit'], 'Rarity']], 10 - hqcount)
+                hqcount += hq_gain
+                if index > self.hqqueue:
+                    self.hqqueue = index
+                    print(f" - HQ Update: {row['Unit']}")
+                if self.unit_gain(row['Unit'], hq_gain):
+                    if self.unitlb['Stage'][row['Unit']] >= LB_SUPPRESS:
+                        print(f"Day {self.day_count}: {row['Unit']} progressed into {STAGE_NAME[self.unitlb.loc[row['Unit'], 'Stage']]}")
+                if hqcount == 10:
+                    break
+    def progress(self, days=360):
+        for _ in range(days):
+            self.daily()
+
+if __name__ == '__main__':
+    my_roster = Roster(myspreadsheet)
+    my_roster.progress()
