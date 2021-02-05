3ff7d16ea067a268fc094e59af3fff72f98aadd1
Shaun <yongfongchuan@gmail.com>
2021-02-05 16:15:47 +1100

    more basic functions engel

--- engelbert.py
+++ engelbert.py
@@ -15,6 +15,7 @@ class Engel:
         self.attack_apcost = 3
         self.hpregen = 0.05
         self.apregen = 0.1
+        self.levelcap = 10
         self.sheettuples = (
             ('Base', 'Base'),
             ('Job', 'JobID'),
@@ -23,6 +24,13 @@ class Engel:
         )
         self.statlist = ('HP', 'AP', 'ATK', 'MAG', 'DEF', 'SPR', 'AGI')
         self.statlist2 = ('ATK', 'MAG', 'DEF', 'SPR', 'AGI')
+        self.statrating = {
+            5: 'A',
+            4: 'B',
+            3: 'C',
+            2: 'D',
+            1: 'E'
+        }
         self.spreadsheet = client.open("Engelbert Bot")
         self.dfdict = dict()
         self.dfsync()
@@ -36,7 +44,9 @@ class Engel:
              self.dfdict[sheetname] = df
     def sheetsync(self, logsync=0):
         # sync local data into online sheet
-        set_with_dataframe(self.spreadsheet.worksheet('User'), self.dfdict['User'], include_index=True)
+        df = self.dfdict['User'].copy()
+        df.index = df.index.astype(str)
+        set_with_dataframe(self.spreadsheet.worksheet('User'), df, include_index=True)
         if logsync:
             set_with_dataframe(self.spreadsheet.worksheet('Log'), self.dfdict['Log'], include_index=False)
     def new_log(self, event, user, timestamp):
@@ -51,10 +61,13 @@ class Engel:
     def jobjp_init(self):
         # initialize job level - JP table
         basejp = 10
-        levelcap = 10
         self.jobjp = dict()
-        for i in range(levelcap):
+        self.jobjpsum = dict()
+        jpsum = 0
+        for i in range(self.levelcap):
             self.jobjp[i] = basejp + (i * (i + 1)) // 2
+            jpsum += basejp + (i * (i + 1)) // 2
+            self.jobjpsum[i] = jpsum
     def calchitrate(self, avoid):
         # calculate hit rate from agi difference
         if avoid < 1:
@@ -86,7 +99,7 @@ class Engel:
             # get their status sheets
             attackdict = self.calcstats(attacker)
             defenddict = self.calcstats(defender)
-            jp_gain = 1 # base JP gain
+            jp_gain = 2 # base JP gain
             # consumes AP
             self.dfdict['User'].loc[attacker, 'AP'] = int(self.dfdict['User'].loc[attacker, 'AP'] - self.attack_apcost)
             # pick higher potential damage
@@ -96,12 +109,11 @@ class Engel:
                 replystr = 'Attack missed!'
             else:
                 replystr = f"{damage} damage!"
-                jp_gain += (damage + defenddict['Level']) // 10 # bonus JP for damage
+                jp_gain += (damage + defenddict['Level']) // 5 # bonus JP for damage
                 kill = self.userdamage(defender, damage)
                 if kill:
                     jp_gain += defenddict['Level'] # bonus JP for killing
-            if self.dfdict['User'].loc[defender, 'AP'] < defenddict['AP']:
-                self.dfdict['User'].loc[defender, 'AP'] = self.dfdict['User'].loc[defender, 'AP'] + 1
+            self.dfdict['User'].loc[defender, 'JP'] = self.dfdict['User'].loc[defender, 'JP'] + 1 + attackdict['Level'] // 10
             replystr += (f"\nGained {jp_gain} JP!")
             self.dfdict['User'].loc[attacker, 'JP'] = self.dfdict['User'].loc[attacker, 'JP'] + jp_gain # gains JP
             self.sheetsync()
@@ -140,6 +152,64 @@ class Engel:
         self.dfdict['Log'].loc[index, 'Event'] = 'userrevived'
         self.dfdict['Log'].loc[index, 'Timestamp'] = datetime.strftime(datetime.now(), mydtformat)
         self.sheetsync(logsync=1)
+    def userjoblevel(self, user, job, level=1):
+        # raises user job levels
+        jobid = self.dfdict['Job'][self.dfdict['Job']['Job'] == job].tail(1).index.tolist()[0]
+        userrow = self.dfdict['User'].loc[user]
+        available_jp = userrow['JP']
+        job_level = userrow[jobid]
+        if job_level == '':
+            job_level = 0
+        job_level_0 = job_level
+        for i in range(level):
+            if job_level == self.levelcap:
+                break
+            if available_jp >= self.jobjp[job_level]:
+                available_jp -= self.jobjp[job_level]
+                job_level += 1
+            else:
+                break
+        if job_level > job_level_0:
+            self.dfdict['User'].loc[user, jobid] = job_level
+            self.dfdict['User'].loc[user, 'JP'] = available_jp
+            self.sheetsync()
+        return f"{job_level - job_level_0} level(s) raised. Your current available JP is now {available_jp}."
+    def userjobreset(self, user):
+        # resets user job
+        userrow = self.dfdict['User'].loc[user]
+        jp_refunded = 0
+        for jobid in self.dfdict['Job'].index:
+            if userrow[jobid] != '':
+                jp_refunded += self.jobjpsum[userrow[jobid]]
+                self.dfdict['User'].loc[user, jobid] = ''
+        if jp_refunded > 0:
+            self.dfdict['User'].loc[user, 'JP'] = self.dfdict['User'].loc[user, 'JP'] + jp_refunded
+            self.new_log('userjobreset', user, datetime.strftime(datetime.now(), mydtformat))
+            self.sheetsync()
+        return f"{jp_refunded} JP refunded!"
+    def userbase(self, user, base):
+        # changes user or starts a user
+        baserow = self.dfdict['Base'].loc[base]
+        if user in self.dfdict['User'].index:
+            if self.dfdict['User'].loc[user, 'Base'] == base:
+                return 'It is your current base!'
+            else:
+                self.dfdict['User'].loc[user, 'Base'] = base
+                replystr = 'Base changed!'
+        else:
+            new_user = {
+                'Base': base,
+                'HP': baserow['HP'],
+                'AP': baserow['AP'],
+                'JP': 0,
+                baserow['Starter']: 1
+            }
+            userrow = pd.Series(data=new_user.values(), index=new_user.keys(), name=user)
+            self.dfdict['User'] = self.dfdict['User'].append(userrow).fillna('')
+            replystr = 'User registered!'
+        self.new_log('userbase', user, datetime.strftime(datetime.now(), mydtformat))    
+        self.sheetsync()
+        return replystr
     ############################
     # discord embed generators #
     ############################
@@ -147,35 +217,90 @@ class Engel:
         # generate embed of list of available bases
         embed = discord.Embed()
         embed.title = 'List of Bases'
+        desc = (
+            'A base determines your base stats and can be changed every 24 hours.',
+            'It also determines your tamagotchi picture where available.',
+            'Element or other features may be implemented in future.'
+        )
+        embed.description = '\n'.join(desc)
         df = self.dfdict['Base'][self.dfdict['Base']['Hidden'] == '']
+        base_list = []
+        base_count = 0
+        for index, row in df.iterrows():
+            disc_list = []
+            for stat in self.statlist2:
+                disc_list.append(f"{stat} `{row[stat]}`")
+            disc_list.append(f"Starter: {self.dfdict['Job'].loc[row['Starter'], 'Job']}")
+            base_list.append(f"**{index}**\n - {' | '.join(disc_list)}")
+            base_count += 1
+            if base_count % 10 == 0:
+                embed.add_field(name='-', value='\n'.join(job_list))
+                base_list = []
+        if len(base_list) > 0:
+            embed.add_field(name='-', value='\n'.join(base_list))
+        return embed
+    def listjob(self):
+        # generate embed of list of available jobs
+        embed = discord.Embed()
+        embed.title = 'List of Jobs'
+        desc = (
+            'Leveling a job costs JP and raises your stats according to the job.',
+            'Higher job levels require more JP.',
+            'Job levels can be reset into JP every 24 hours.',
+            'Skills or other features may be implemented in future.'
+        )
+        embed.description = '\n'.join(desc)
+        df = self.dfdict['Job']
         job_list = []
         job_count = 0
         for index, row in df.iterrows():
             disc_list = []
             for stat in self.statlist2:
-                disc_list.append(f"{stat}: {row[stat]}")
-            disc_list.append(f"Starting Job: {row['Job']}")
-            job_list.append(f"**{index}**: {' | '.join(disc_list)}")
+                disc_list.append(f"{stat} `{self.statrating[row[stat]]}`")
+            job_list.append(f"**{row['Job']}**\n - {' | '.join(disc_list)}")
             job_count += 1
             if job_count % 10 == 0:
-                if job_count // 10 > 0:
-                    embed.add_field(name='Cont.', value='\n'.join(job_list))
-                else:
-                    embed.description = '\n'.join(job_list)
+                embed.add_field(name='-', value='\n'.join(job_list))
                 job_list = []
         if len(job_list) > 0:
-            if job_count // 10 > 0:
-                embed.add_field(name='Cont.', value='\n'.join(job_list))
-            else:
-                embed.description = '\n'.join(job_list)
+            embed.add_field(name='-', value='\n'.join(job_list))
         return embed
-    def infouser(self, row):
+    def infojp(self, user):
+        # generate info embed of specific user jobs and jp
+        embed = discord.Embed()
+        userrow = self.dfdict['User'].loc[user]
+        embed.title = f"{userrow.name} Jobs" # convert ID into actual name...
+        embed.description = f"JP: {userrow['JP']}"
+        dfjob = self.dfdict['Job']
+        job_list = []
+        job_count = 0
+        for index, row in dfjob.iterrows():
+            job_level = userrow[index]
+            if job_level == '':
+                job_level = 0
+            if job_level < self.levelcap:
+                next_jp = self.jobjp[job_level]
+                jobstr = f"**{row['Job']}**\n - Level `{job_level}` (Next: `{next_jp}` JP)"
+                if userrow['JP'] >= next_jp:
+                    jobstr += ' **OK**'
+                job_list.append(jobstr)
+            else:
+                job_list.append(f"**{row['Job']}**\n - Level `{job_level}` (MAX)")
+            job_count += 1
+            if job_count % 10 == 0:
+                embed.add_field(name='-', value='\n'.join(job_list))
+                job_list = []
+        if len(job_list) > 0:
+            embed.add_field(name='-', value='\n'.join(job_list))
+        return embed
+    def infouser(self, user):
         # generate info embed of specific user
         embed = discord.Embed()
-        embed.title = row.name
+        row = self.dfdict['User'].loc[user]
+        embed.title = row.name # convert ID into actual name...
         disc_list = []
-        disc_list.append(f"Base: {row['Base']} (+{userdict['Level']})")
         userdict = self.calcstats(row.name)
+        disc_list.append(f"Base: {row['Base']} (+{userdict['Level']})")
         disc_list.append(f"HP: {row['HP']}/{userdict['HP']}")
         disc_list.append(f"AP: {row['AP']}/{userdict['AP']}")
         disc_list.append(f"JP: {row['JP']}")
@@ -222,38 +347,53 @@ class Engelbert(commands.Cog):
             if arg[0] == 'info':
                 if len(arg) == 1:
                     # own info
-                    try:
-                        row = engel.dfdict['User'].loc[ctx.author.id]
-                        await ctx.send(engel.infouser(row))
-                    except KeyError:
-                        await ctx.send('Nice try 2.') # to be implemented with proper starter stuff
+                    user = ctx.author.id
                 else:
-                    try:
-                        # to be implemented with finding member
-                        row = engel.dfdict['User'].loc[arg[1]]
-                        await ctx.send(embed = engel.infouser(row))
-                    except KeyError:
-                        await ctx.send('Nice try 3.') # to be implemented with proper suggestion
+                    member = await commands.MemberConverter().convert(ctx, ' '.join(arg[1:]))
+                    user = member.id
+                if user in engel.dfdict['User'].index:
+                    await ctx.send(embed = engel.infouser(user))
+                else:
+                    await ctx.send('Nice try 2.') # to be implemented with proper starter stuff
             elif arg[0] == 'base':
                 if len(arg) == 1:
                     # list of bases
                     await ctx.send(embed = engel.listbase())
                 else:
                     # various operations
-                    # find base and info
-                    # change base
-                    # start of tamagotchi
-                    pass
+                    if len(arg) > 2 and arg[1] in ('change', 'start'):
+                        # change base or start of tamagotchi
+                        user = ctx.author.id
+                        base = ' '.join(arg[2:])
+                        if base in engel.dfdict['Base'].index:
+                            await ctx.send(engel.userbase(user, base))
+                        else:
+                            await ctx.send('Nice try 3.') # to be implemented with proper suggestion
+                    else:
+                        # find base and info (not needed atm)
+                        await ctx.send('Nice try 4.') # to be implemented with proper suggestion
             elif arg[0] == 'job':
                 if len(arg) == 1:
                     # list of jobs
-                    pass
+                    await ctx.send(embed = engel.listjob())
                 else:
-                    # various operations
-                    # find job and info
-                    # level job
-                    # reset job
-                    pass
+                    user = ctx.author.id
+                    if arg[1] in ('level',):
+                        if len(arg) == 2:
+                            await ctx.send(embed = engel.infojp(user))
+                        else:
+                            job = ' '.join(arg[2:])
+                            await ctx.send(engel.userjoblevel(user, job))
+                    elif arg[1] in ('levels',):
+                        if len(arg) > 3:
+                            num_levels = int(arg[2])
+                            job = ' '.join(arg[3:])
+                            await ctx.send(engel.userjoblevel(user, job, num_levels))
+                    elif arg[1] in ('reset',):
+                        await ctx.send(engel.userjobreset(user))
+                    else:
+                        # find job and info (not needed atm)
+                        await ctx.send('Nice try 5.') # to be implemented with proper suggestion
             elif arg[0] == 'attack':
                 if len(arg) == 1:
                     # ???
